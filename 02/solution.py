d188b1a1c907a937fde0bffeb2340257cad9faee
Anès Foufa <anesfoufa@proton.me>
2024-12-02 22:24:56 +0100

    day 2 in python

---
+++ 02/solution.py
@@ -0,0 +1,53 @@
+from sys import argv
+
+
+def part_one(path):
+    solution(path, is_safe)
+
+
+def part_two(path):
+    solution(path, is_safe_with_tolerance)
+
+
+def solution(path, is_safe_func):
+    res = 0
+    for level in read_levels(path):
+        if is_safe_func(level):
+            res += 1
+    print(res)
+
+
+def read_levels(path):
+    with open(path, "r") as f:
+        for line in f:
+            yield [int(l) for l in line.strip().split()]
+
+
+def is_safe(levels):
+    return is_moderately_increasing(levels) or is_moderately_increasing(levels[::-1])
+
+
+def is_safe_with_tolerance(levels):
+    if is_safe(levels):
+        return True
+    return any(is_safe(truncated_level) for truncated_level in truncate_levels(levels))
+
+
+def is_moderately_increasing(levels):
+    growth_rates = (
+        next_level - current_level
+        for (current_level, next_level) in zip(levels[:-1], levels[1:])
+    )
+    res = all(1 <= growth_rate <= 3 for growth_rate in growth_rates)
+    return res
+
+
+def truncate_levels(levels):
+    for i, _ in enumerate(levels):
+        yield levels[:i] + levels[i + 1 :]
+
+
+if __name__ == "__main__":
+    path = argv[1]
+    part_one(path)
+    part_two(path)
